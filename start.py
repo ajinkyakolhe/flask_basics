0c6f539e69704f49b17a688994483f4e0ec514f8
ajinkya <ajinkya.kolhe@yahoo.com>
2020-01-11 17:08:27 -0800

    Minor changes in comments

--- start.py
+++ start.py
@@ -3,11 +3,11 @@ A Flask python program that creates routes for three different types
 of pages with the help of Flask web framework
 """
 
-
 from flask import Flask, redirect, url_for
 
 # Creating an object for using the Flask module
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
@@ -15,19 +15,20 @@ def home():
     return "Hello, this is the first page <h1>Hey</h1>"
 
 
-
 @app.route("/admin")
-
 def admin():
-    """Redirects the admin page to the home page"""
+    """Redirects the admin page to the home page
+    :rtype: webpage
+    """
     return redirect(url_for("home"))
+
 
 @app.route("/<name>")
 def user(name):
     """Returns the string from the page name provided"""
     return f"Hello {name}!"
 
+
 # Main function to run the Flask app
 if __name__ == "__main__":
     app.run()
-
